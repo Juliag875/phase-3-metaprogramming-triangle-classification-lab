40542308da0f3fa8b1b288c7350273611ce24723
Iuliia El Sammak <juliag875@gmail.com>
2022-03-06 20:27:37 -0500

    commit tree 4d8f9dcbe96495bbccc2526d59f74e7bff4f34f6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
   # write code here
+  attr_reader :x, :y, :z
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    if x==y && x==z
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    elsif
+      :scalene
+    else 
+      begin
+        raise PartnerError 
+      resque PartnerError => error
+        puts error.message
+      end  
+    end
+  end
+
+  # def no_size
+
+  # end
+
+ class TriangleError < StandardError
+    def message
+      "error"
+    end
+ end
 end
